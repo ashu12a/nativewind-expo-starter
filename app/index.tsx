002ad69fbf7e1ae7157927bf0cea5a6c205b73c2
ashu12a <ashutoshsharma86416@gmail.com>
2025-04-02 11:15:21 +0530

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function index() {
+  return (
+    <View className='bg-green-600'>
+      <Text className='text-red-500 text-4xl'>index</Text>
+    </View>
+  )
+}
